949cf8fd4311432b8f579a6b0759416c5743eda9
GMwasi <mwasi.brian@thepalladiumgroup.com>
2018-02-07 12:26:18 +0300

    Added the sdk jar

---
+++ psmartlibrary/src/test/java/org/kenyahmis/psmartlibrary/AcsTest.java
@@ -0,0 +1,16 @@
+package org.kenyahmis.psmartlibrary;
+
+import org.junit.Assert;
+import org.junit.Test;
+import com.acs.audiojack.*;
+
+/**
+ * Created by GMwasi on 2/7/2018.
+ */
+
+public final class AcsTest {
+    @Test
+    public void Reader_IsOnline(){
+
+    }
+}
