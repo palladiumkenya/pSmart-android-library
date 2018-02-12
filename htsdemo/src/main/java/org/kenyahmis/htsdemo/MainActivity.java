e00c7d23feda417c7eb5aa427f03e4d3edfdca94
GMwasi <mwasi.brian@thepalladiumgroup.com>
2018-02-12 17:03:29 +0300

    imported activity to the demo app

--- htsdemo/src/main/java/org/kenyahmis/htsdemo/MainActivity.java
+++ htsdemo/src/main/java/org/kenyahmis/htsdemo/MainActivity.java
@@ -1,13 +1,19 @@
 package org.kenyahmis.htsdemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-public class MainActivity extends AppCompatActivity {
 
-    @Override
+import org.kenyahmis.psmartlibrary.Readers.Acr3x;
+
+public class MainActivity extends Acr3x {
+
+    /*@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        Intent intent = new Intent(this, Acr3x.class);
+        startActivity(intent);
+        setContentView(org.kenyahmis.psmartlibrary.R.layout.activity_acr3x);
 
-    }
+    }*/
 }
