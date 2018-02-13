83002f97004783dbc8d0e7b6ca6f3c701893f661
GMwasi <mwasi.brian@thepalladiumgroup.com>
2018-02-13 09:38:38 +0300

    completed mock hts demo app layout

---
+++ htsdemo/src/main/java/org/kenyahmis/htsdemo/WriteCardActivity.java
@@ -0,0 +1,10 @@
+package org.kenyahmis.htsdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.kenyahmis.psmartlibrary.Readers.Acr3x;
+
+public class WriteCardActivity extends Acr3x {
+
+}
