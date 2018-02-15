f331397ecb6b545109327a7ceffaf532272bac13
GMwasi <mwasi.brian@thepalladiumgroup.com>
2018-02-15 13:49:57 +0300

    hex to string and vice versa methods

--- psmartlibrary/src/main/java/org/kenyahmis/psmartlibrary/Util.java
+++ psmartlibrary/src/main/java/org/kenyahmis/psmartlibrary/Util.java
@@ -8,6 +8,7 @@ import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Locale;
 
 /**
  * Created by GMwasi on 2/14/2018.
@@ -160,6 +161,118 @@ public class Util {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * Creates a hexadecimal <code>String</code> representation of the
+     * <code>byte[]</code> passed. Each element is converted to a
+     * <code>String</code> via the {@link Integer#toHexString(int)} and
+     * separated by <code>" "</code>. If the array is <code>null</code>, then
+     * <code>""<code> is returned.
+     *
+     * @param array
+     *            the <code>byte</code> array to convert.
+     * @return the <code>String</code> representation of <code>array</code> in
+     *         hexadecimal.
+     */
+    public static String ByteArrayToHexString(byte[] array) {
+
+        String bufferString = "";
+
+        if (array != null) {
+            for (int i = 0; i < array.length; i++) {
+                String hexChar = Integer.toHexString(array[i] & 0xFF);
+                if (hexChar.length() == 1) {
+                    hexChar = "0" + hexChar;
+                }
+                bufferString += hexChar.toUpperCase(Locale.US) + " ";
+            }
+        }
+        return bufferString;
+    }
+
+    private static boolean isHexNumber(byte value) {
+        if (!(value >= '0' && value <= '9') && !(value >= 'A' && value <= 'F')
+                && !(value >= 'a' && value <= 'f')) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Checks a hexadecimal <code>String</code> that is contained hexadecimal
+     * value or not.
+     *
+     * @param string
+     *            the string to check.
+     * @return <code>true</code> the <code>string</code> contains Hex number
+     *         only, <code>false</code> otherwise.
+     * @throws NullPointerException
+     *             if <code>string == null</code>.
+     */
+    public static boolean isHexNumber(String string) {
+        if (string == null)
+            throw new NullPointerException("string was null");
+
+        boolean flag = true;
+
+        for (int i = 0; i < string.length(); i++) {
+            char cc = string.charAt(i);
+            if (!isHexNumber((byte) cc)) {
+                flag = false;
+                break;
+            }
+        }
+        return flag;
+    }
+
+    private static byte uniteBytes(byte src0, byte src1) {
+        byte _b0 = Byte.decode("0x" + new String(new byte[] { src0 }))
+                .byteValue();
+        _b0 = (byte) (_b0 << 4);
+        byte _b1 = Byte.decode("0x" + new String(new byte[] { src1 }))
+                .byteValue();
+        byte ret = (byte) (_b0 ^ _b1);
+        return ret;
+    }
+
+    /**
+     * Creates a <code>byte[]</code> representation of the hexadecimal
+     * <code>String</code> passed.
+     *
+     * @param string
+     *            the hexadecimal string to be converted.
+     * @return the <code>array</code> representation of <code>String</code>.
+     * @throws IllegalArgumentException
+     *             if <code>string</code> length is not in even number.
+     * @throws NullPointerException
+     *             if <code>string == null</code>.
+     * @throws NumberFormatException
+     *             if <code>string</code> cannot be parsed as a byte value.
+     */
+    public static byte[] HexStringToByteArray(String string) {
+        if (string == null)
+            throw new NullPointerException("string was null");
+
+        int len = string.length();
+
+        if (len == 0)
+            return new byte[0];
+        if (len % 2 == 1)
+            throw new IllegalArgumentException(
+                    "string length should be an even number");
+
+        byte[] ret = new byte[len / 2];
+        byte[] tmp = string.getBytes();
+
+        for (int i = 0; i < len; i += 2) {
+            if (!isHexNumber(tmp[i]) || !isHexNumber(tmp[i + 1])) {
+                throw new NumberFormatException(
+                        "string contained invalid value");
+            }
+            ret[i / 2] = uniteBytes(tmp[i], tmp[i + 1]);
+        }
+        return ret;
     }
 
 }
